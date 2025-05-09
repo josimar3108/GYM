688eaf4f5ab40ebc3dd85f8dadcc482f64fe87f8
Gustavo Andrés Mojica Lamas <58308130+WeroML@users.noreply.github.com>
2025-05-08 22:20:34 -0600

    Add files via upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,12 @@ import { Component } from '@angular/core';
 import { RouterModule, RouterOutlet } from '@angular/router';
 import { IndexComponent } from './components/index/index.component';
 import { LoginComponent } from './components/login/login.component';
+import { FooterComponent } from "./components/footer/footer.component";
+import { HeaderComponent } from "./components/header/header.component";
 
 @Component({
   selector: 'app-root',
-  imports: [IndexComponent, LoginComponent],
+  imports: [IndexComponent, LoginComponent, FooterComponent, HeaderComponent,RouterModule,RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,14 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
+import { IndexComponent } from './components/index/index.component';
+import { ServiciosComponent } from './servicios/servicios.component';
+import { ContactoComponent } from './contacto/contacto.component';
 
 export const routes: Routes = [
+  {path: 'index', component: IndexComponent},
   { path: 'login', component: LoginComponent },
-  { path: '', redirectTo: '/login', pathMatch: 'full' }
+  { path: 'servicios', component: ServiciosComponent },
+  { path: 'contacto', component: ContactoComponent },
+  { path: '', redirectTo: '/index', pathMatch: 'full' }
   
 ];
